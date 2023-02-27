fe453044eb80c77692b07de3aea77538679308e5
Andreas Fenkart <andreas.fenkart@digitalstrom.com>
2023-02-27 09:34:25 +0100

    parproute-test: initial

---
+++ Readme.txt
@@ -0,0 +1,6 @@
+
+meson setup --reconfigure --native-file native.ini build
+
+https://roy.marples.name/projects/parpd/
+https://www.rfc-editor.org/rfc/rfc1027
+https://www.gsp.com/cgi-bin/man.cgi?section=8&topic=parpd
--- src/parprouted-test.cpp
+++ src/parprouted-test.cpp
@@ -1,9 +1,17 @@
 #include <catch2/catch_all.hpp>
 
+#include "parprouted.h"
+
 namespace {
 
 constexpr const char *TAGS = "foo";
 
-TEST_CASE("parparouted-test", TAGS) {}
+TEST_CASE("parprouted-test", TAGS) {
+
+  arptab_entry *arptab_head = nullptr;
+  arptab = &arptab_head; // global list head
+
+  SECTION("parseproc") { parseproc(); }
+}
 
 } // namespace
