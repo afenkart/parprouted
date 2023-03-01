7959a923dd1d10ee0943ab9347727c4a7a43138b
Andreas Fenkart <andreas.fenkart@digitalstrom.com>
2023-03-01 12:37:01 +0100

    parse firstline

--- src/parprouted-test.cpp
+++ src/parprouted-test.cpp
@@ -6,6 +6,7 @@
 namespace {
 
 using trompeloeil::_;
+using namespace trompeloeil;
 
 constexpr const char *TAGS = "foo";
 
@@ -15,7 +16,16 @@ TEST_CASE("parprouted-test", TAGS) {
     REQUIRE_CALL(fileSystem, fopen(_, _))
         .RETURN(reinterpret_cast<FILE *>(0xdeadbeef));
     REQUIRE_CALL(fileSystem, feof(_)).RETURN(false);
+    REQUIRE_CALL(fileSystem, fgets(_, _, _))
+        .LR_SIDE_EFFECT(
+            auto constexpr firstline =
+                R"(IP address       HW type     Flags       HW address            Mask     Device)";
+            std::strcpy(_1, firstline))
+        .RETURN(_1);
     parseproc(fileSystem);
+#if 0
+    192.168.11.182   0x1         0x2         00:1e:74:00:4a:88     *        wlp58s0
+#endif
   }
 }
 
