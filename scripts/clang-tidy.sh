3efd6d22f3fd4bf63de09f8fbe2630e064948280
Andreas Fenkart <andreas.fenkart@digitalstrom.com>
2023-04-17 10:41:28 +0200

    clang-tidy: check for missing braces around statements

--- scripts/clang-tidy.sh
+++ scripts/clang-tidy.sh
@@ -6,8 +6,8 @@ PROJECT_ROOT=$1 && shift
 BUILD_ROOT=$1 && shift
 
 # Pick any flags you like here
-CHECKS='-hicpp-*,-readability-implicit-bool-conversion,-cppcoreguidelines-*,-clang-diagnostic*,-llvm-*,-bugprone-*,-modernize-*,-misc-*'
-# CHECKS='*'
+# CHECKS='-hicpp-*,-readability-implicit-bool-conversion,-cppcoreguidelines-*,-clang-diagnostic*,-llvm-*,-bugprone-*,-modernize-*,-misc-*'
+CHECKS='readability-braces-around-statements'
 
 # Execute in a different directory to ensure we don't mess with the meson config
 TIDY_DIR=${PROJECT_ROOT}/build-tidy
