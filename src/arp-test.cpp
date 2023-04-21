ff9139ce4c4e33a8b874a0ce9a552349bc93c655
Andreas Fenkart <andreas.fenkart@digitalstrom.com>
2023-04-21 09:44:41 +0200

    arp-test: mockup sockaddr in arp_reply unit test

--- src/arp-test.cpp
+++ src/arp-test.cpp
@@ -20,9 +20,20 @@ TEST_CASE("arp-test", TAGS) {
 
   SECTION("arp_reply") {
     ether_arp_frame frame{ether_header{}, ether_arp{}};
+
+    struct sockaddr_ll ifs {                //
+      .sll_family = AF_PACKET,              //
+          .sll_protocol = htons(ETH_P_ARP), //
+          .sll_ifindex = 7,                 //
+          .sll_hatype = ARPHRD_ETHER,       //
+          .sll_pkttype = PACKET_BROADCAST,  //
+          .sll_halen = ETH_ALEN,            //
+          .sll_addr = {01, 02, 03, 04, 05, 06, 07, 0x08},
+    };
+
     REQUIRE_CALL(context, socket(AF_PACKET, SOCK_RAW, htons(ETH_P_ARP))).RETURN(11);
     REQUIRE_CALL(context, bind(11, _, sizeof(sockaddr_ll))).RETURN(0);
-    arp_reply(&frame, nullptr, context);
+    arp_reply(&frame, &ifs, context);
   }
 }
 
